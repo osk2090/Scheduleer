1206b758ab4b3938c5709e0e68f9769e10f780c7
osk2090 <osk2090@naver.com>
2022-03-03 23:33:12 +0900

    first commit

---
+++ src/main/java/com/www/scheduleer/ScheduleerApplication.java
@@ -0,0 +1,13 @@
+package com.www.scheduleer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleerApplication.class, args);
+    }
+
+}
