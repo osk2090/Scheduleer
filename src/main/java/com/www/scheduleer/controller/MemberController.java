09f7720c00a69223335556d970ff34fc61c88636
osk2090 <osk2090@naver.com>
2022-04-26 22:58:56 +0900

    개인정보 수정 기능 작성중

--- src/main/java/com/www/scheduleer/controller/MemberController.java
+++ src/main/java/com/www/scheduleer/controller/MemberController.java
@@ -10,11 +10,9 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
