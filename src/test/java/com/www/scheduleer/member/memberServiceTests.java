2feb9ccd0f9709b6aae1fd10a00289f72a9a1125
osk2090 <osk2090@naver.com>
2023-01-01 02:29:15 +0900

    GCS 연결 완료 및 이미지 업로드 테스트 완료

--- src/test/java/com/www/scheduleer/member/memberServiceTests.java
+++ src/test/java/com/www/scheduleer/member/memberServiceTests.java
@@ -4,15 +4,12 @@ import com.google.cloud.storage.BlobInfo;
 import com.www.scheduleer.domain.UploadReqDto;
 import com.www.scheduleer.service.Member.GCSService;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Log4j2
@@ -21,12 +18,16 @@ public class memberServiceTests {
     private GCSService gcsService;
 
     @Test
+    @DisplayName("GCS 업로드 테스트")
     public void uploadProfile() throws IOException {
+        long stime = System.currentTimeMillis();
+
         UploadReqDto dto = new UploadReqDto();
         dto.setBucketName("scheduleer");
-        dto.setUploadFileName("uploadGCS/profile/profile-test-file.png");
+        dto.setUploadFileName("uploadGCS/profile/profile-test-file.png");// 유저 email별로 파일 받기
         dto.setLocalFileLocation("/Users/osk2090/Documents/Git/Scheduleer/src/main/resources/static/image/profile-test-file.png");
-        BlobInfo fileFromGCS = gcsService.uploadFileToGCS(dto);
-        assertThat("--- 프로필 업로드 완료! ---");
+        BlobInfo blobInfo = gcsService.uploadFileToGCS(dto);
+
+        System.out.println("소요시간:"+(System.currentTimeMillis()-stime)+"ms");
     }
 }
