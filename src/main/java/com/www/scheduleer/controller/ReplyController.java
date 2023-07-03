eb09d4126da759a4a9a0f1d912bb8cd9f2c0b1d8
osk2090 <osk2090@naver.com>
2023-07-03 22:44:16 +0900

    add reply push noti

--- src/main/java/com/www/scheduleer/controller/ReplyController.java
+++ src/main/java/com/www/scheduleer/controller/ReplyController.java
@@ -4,6 +4,7 @@ import com.www.scheduleer.config.annotation.CurrentMember;
 import com.www.scheduleer.controller.dto.board.ReplySaveDto;
 import com.www.scheduleer.domain.Member;
 import com.www.scheduleer.service.Board.ReplyService;
+import com.www.scheduleer.service.kafka.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReplyController {
 
     private final ReplyService replyService;
+    private final KafkaProducer producer;
 
     @PostMapping("/add")
     public Long addReply(ReplySaveDto replySaveDto, @CurrentMember Member member) {
+        producer.sendMessage(member.getNickName()+" 님이 댓글을 등록하였습니다!");
+
         return replyService.save(replySaveDto, member);
     }
 }
